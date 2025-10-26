bb937f525ba03c3d0c5a3a543fdbfb425d73288e
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2025-10-27 00:28:38 +0300

    edit responsiveness

--- src/app/admin/components/ProductDetail.tsx
+++ src/app/admin/components/ProductDetail.tsx
@@ -20,7 +20,7 @@ export default function ProductDetail({
   return (
     <div className="border-b pb-6 last:border-b-0 bg-gray-200 rounded-2xl p-6">
       <div
-        className="flex justify-between items-center gap-3 cursor-pointer"
+        className="flex justify-between md:flex-row flex-col md:items-center gap-3 cursor-pointer"
         role="button"
         aria-expanded={isExpanded}
         onClick={onToggle}
@@ -36,14 +36,16 @@ export default function ProductDetail({
             <p className="text-xs sm:text-sm text-gray-500">
               Qty: {product.quantity} • Kilo: {product.kilo} kg
             </p>
-            <p className="text-xs sm:text-sm text-gray-600 italic">
+            <p className="text-xs sm:text-sm line-clamp-1 text-gray-600 italic">
               {product.message
                 ? `Message: ${truncateMessage(product.message, 40)}`
                 : "—"}
             </p>
           </div>
         </div>
-        <p className="font-semibold text-sm">${product.price.toFixed(2)}</p>
+        <p className="font-semibold text-xs text-right">
+          ${product.price.toFixed(2)}
+        </p>
       </div>
 
       {isExpanded && (
@@ -63,8 +65,9 @@ export default function ProductDetail({
                 <p className="font-medium">
                   ${(product.price * product.quantity).toFixed(2)}
                 </p>
+
                 <p className="text-xs text-gray-500">
-                  ${product.price.toFixed(2)} ea
+                  ${product.price.toFixed(2)}
                 </p>
               </div>
             </div>
