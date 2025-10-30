3668d85ed53d3ad68a6a7aedf6b7947707f219be
abdulazizgr <abdulazizisa579@gmail.com>
2025-10-30 19:26:57 +0300

    refactor: Update background color in About and Contact pages to use consistent variable

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import { Header, PageHeader } from "@/components";
 
 export default function AboutPage() {
   return (
-    <div className="min-h-screen bg-[#FFFAFF]">
+    <div className="min-h-screen bg-background">
       <PageHeader
         title="About Sweet Delights"
         subtitle="Creating sweet memories since 2020"
