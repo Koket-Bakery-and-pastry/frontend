f9290663c54fc476ae74d02170cf9669a0d45fb9
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2025-11-08 12:00:41 +0300

    add product add page

--- src/app/data/mockCategories.ts
+++ src/app/data/mockCategories.ts
@@ -1,47 +1,53 @@
-import { Category } from "../types/category";
+// import { Category } from "../types/category";
 
-export const initialCategories: Category[] = [
-  {
-    id: "1",
-    name: "Cakes",
-    subcategories: [
-      {
-        id: "1-1",
-        name: "Moca cakes",
-        parentCategory: "1",
-        createdAt: "2024-01-01",
-      },
-      {
-        id: "1-2",
-        name: "Black forest cakes",
-        parentCategory: "1",
-        createdAt: "2024-01-01",
-      },
-      {
-        id: "1-3",
-        name: "Cup cakes",
-        parentCategory: "1",
-        createdAt: "2024-01-01",
-      },
-    ],
-    createdAt: "2024-01-01",
-  },
-  {
-    id: "2",
-    name: "Quick Bread",
-    subcategories: [],
-    createdAt: "2024-01-01",
-  },
-  {
-    id: "3",
-    name: "Cookies",
-    subcategories: [],
-    createdAt: "2024-01-01",
-  },
-  {
-    id: "4",
-    name: "Fondant",
-    subcategories: [],
-    createdAt: "2024-01-01",
-  },
-];
+// export const initialCategories: Category[] = [
+//   {
+//     _id: "1",
+//     name: "Cakes",
+//     subcategories: [
+//       {
+//         _id: "1-1",
+//         name: "Moca cakes",
+//         category_id: "1",
+//         status: "available",
+//         upfront_payment: 500,
+//         created_at: "2024-01-01",
+//       },
+//       {
+//         _id: "1-2",
+//         name: "Black forest cakes",
+//         category_id: "1",
+//         status: "available",
+//         upfront_payment: 500,
+//         created_at: "2024-01-01",
+//       },
+//       {
+//         _id: "1-3",
+//         name: "Cup cakes",
+//         category_id: "1",
+//         status: "available",
+//         upfront_payment: 500,
+//         created_at: "2024-01-01",
+//       },
+//     ],
+//     createdAt: "2024-01-01",
+//   },
+//   {
+//     _id: "2",
+//     name: "Quick Bread",
+//     subcategories: [],
+//     createdAt: "2024-01-01",
+//   },
+//   {
+//     _id: "3",
+//     name: "Cookies",
+//     subcategories: [],
+//     createdAt: "2024-01-01",
+//   },
+//   {
+//     _id: "4",
+//     name: "Fondant",
+//     subcategories: [],
+//     createdAt: "2024-01-01",
+//   },
+// ];
